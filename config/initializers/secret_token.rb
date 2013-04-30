e6e50e5ae9dc0b48ddcb32c17ad0734b895143be
rbn <richnyman@gmail.com>
2013-04-29 21:25:06 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BizDevSim::Application.config.secret_token = 'f8135093c548ccc59a65be329be290d9e2c6ea5e4361396632e346e28fc7525ee22f9df83d83beca3275e344d7b4d76423b6380290534ffd44936cce028f18a6'

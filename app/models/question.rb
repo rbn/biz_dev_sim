64318c642de7d0a8712881fe914a3a5659a5514c
rbn <richnyman@gmail.com>
2013-06-09 15:11:00 -0400

    fixing question model initialization/answerset

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,13 +1,9 @@
 class Question < ActiveRecord::Base
   attr_accessible :text, :answers, :html_style
   belongs_to :stage
-  after_initialize :init
-
-  def init
-    @answer_set = AnswerSet.new(answers)
-  end
 
   def answer_set
+    @answer_set = AnswerSet.new(answers) unless @answer_set
     @answer_set
   end
 end
--- db/migrate/20130608162413_create_questions.rb
+++ db/migrate/20130608162413_create_questions.rb
@@ -2,7 +2,7 @@ class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
       t.string :text
-      t.text :answers
+      t.text :answers, limit: nil
       t.string :html_style, default: 'radio'
       t.integer :stage_id
       t.references :stage
